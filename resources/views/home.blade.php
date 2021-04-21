904037f963e5a78056fac81d5ef1e7ebec3e2a87
alainjr10 <njokomalain@gmail.com>
2021-04-21 12:57:53 +0100

    Free code gram first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://res-4.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco/ikqra03zdnggljdu5vv0" alt=" " style="height: 140px;" class="rounded-circle" >
+        </div>
+        <div class="col-9 pt-5"> 
+             <div><h1>freecodecamp</h1> </div>
+             <div class="d-flex">
+                 <div class="pr-5"><strong>372</strong> posts </div>
+                 <div class="pr-5"><strong>68.3k</strong> followers </div>
+                 <div class="pr-5"><strong>309</strong> following </div>
+             </div>
+        </div>
+    </div>
+</div>
+@endsection
